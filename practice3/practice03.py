a54a297e186f628f2732adf3edc6c1dddc2e7dbf
Alex.LastSurvivour <last.sur.90@gmail.com>
2024-03-26 21:30:33 +0500

    Added practice 03 to the folder

---
+++ practice3/practice03.py
@@ -0,0 +1,19 @@
+# # Task 13. Ottepel
+# daysCount = int( input ("Input the amount of days here: ", ))
+# count = 0
+# maxcount = 0
+# print(daysCount)
+# if daysCount >= 1 and daysCount <= 100:
+#     for temperature in range(daysCount):
+#         temperature = int(input ("Input the temperature of each day: ", ))
+#         if temperature > 0:
+#             count += 1
+#             if count >= maxcount:
+#                 maxcount = count
+#         else:
+#             if count >= maxcount:
+#                 maxcount = count
+#             count = 0
+#     print ("The amount of days with temperature > 0 is ", maxcount)
+
+# Task 15. Watermelons (AKA weights) 48: 08
