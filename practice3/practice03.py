3f174dfacbc26dc6c834d1fbaa522a05e97051ec
Alex.LastSurvivour <last.sur.90@gmail.com>
2024-03-29 23:42:50 +0500

    Added solution for task 15 at practice 3

--- practice3/practice03.py
+++ practice3/practice03.py
@@ -6,14 +6,41 @@
 # if daysCount >= 1 and daysCount <= 100:
 #     for temperature in range(daysCount):
 #         temperature = int(input ("Input the temperature of each day: ", ))
-#         if temperature > 0:
+#         if temperature >0:
 #             count += 1
-#             if count >= maxcount:
-#                 maxcount = count
 #         else:
-#             if count >= maxcount:
-#                 maxcount = count
+#             maxcount = max (maxcount, count)
 #             count = 0
+#     maxcount = max (maxcount, count)
+        
+#         # temperature = int(input ("Input the temperature of each day: ", ))
+#         # if temperature > 0:
+            
+#         #     count += 1
+#         #     if count >= maxcount:
+#         #         maxcount = count
+#         # else:
+#         #     if count >= maxcount:
+#         #         maxcount = count
+#         #         count = 0
 #     print ("The amount of days with temperature > 0 is ", maxcount)
 
-# Task 15. Watermelons (AKA weights) 48: 08
+# # Task 15. Watermelons (AKA weights) 48: 08
+# melonsCount = int( input ("Input the amount of watermelons here: ", ))
+# print(melonsCount)
+
+# for i in range(melonsCount):
+#     weight = int(input ("Input the weight of each watermelon: ", ))
+#     if weight > 0:
+#         if i == 0:
+#             melonMin = weight
+#             melonMax = weight
+#         elif weight < melonMin:
+#             melonMin = weight
+#         elif weight > melonMax:
+#             melonMax = weight
+#     else:
+#         print("mellons cannot weight zero")
+# print(melonMin, melonMax)
+
+
